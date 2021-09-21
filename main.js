94f119094a18de9c6e7fc09d75c23b34b8736cee
Rainbowshine295 <79471824+Rainbowshine295@users.noreply.github.com>
2021-09-21 12:44:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+rightWristY = "";
+rightWristX = "";
+scoreRightWrist = "";
+
+
 function preload()
 {
 
@@ -12,12 +17,30 @@ video.size(700, 600);
 video.hide();
 
 poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose' , gotPoses);
 }
 
 function modelLoaded() {
   console.log("Model loaded! :)");
 }
 
+function gotPoses(results)
+{
+  if(results.length > 0) {
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
 function draw() {
     image(video , 0 , 0 , 700,600);
+    if(scoreRightWrist > 0.2)
+  {
+    fill("aquamarine");
+    stroke("aquamarine");
+    circle(rightWristX, rightWristY, 35);
+  }
+
 }
